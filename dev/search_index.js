87101b8b2687792f80c4e54decc20fa441ed7a8f
Documenter.jl <documenter@juliadocs.github.io>
2021-02-16 12:04:45 +0000

    build based on 6cd96a3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TeaEM","category":"page"},{"location":"#TeaEM","page":"Home","title":"TeaEM","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TeaEM]","category":"page"}]
+}
